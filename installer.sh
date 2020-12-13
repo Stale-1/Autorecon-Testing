fcc5a4a181f8065a70c394b5d745c83ed23899c4
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2020-12-13 23:18:29 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,9 +6,10 @@ wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/m
 wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/menu/port.sh -O /root/stale/port.sh && chmod +x port.sh
 wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/.stale.sh -O /root/.stale.sh && chmod +x .stale.sh
 wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/.bash_aliases -O /root/.bash_aliases
-wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/autorecon/recon -O /root/recon.sh && chmod +x recon.sh
+wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/autorecon/recon -O /root/stale.sh && chmod +x stale.sh
 wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/autorecon/sleep -O /root/stale/.sleep.sh && chmod +x .sleep.sh
 wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/autorecon/kill -O /root/stale/.kill.sh && chmod +x .kill.sh
+wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/autorecon/merge -O /root/stale/.merge.sh && chmod +x .merge.sh
 echo "Tasks Finished!!!"
 echo ""
 echo "To configure the Autorecon"
