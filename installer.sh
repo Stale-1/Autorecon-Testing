d0092bbf69ce8a4104bc28c2324127c33ed15071
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2020-12-13 22:10:38 +0800

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Downloading necessary files"
+wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/menu/timer.sh -O /root/stale/timer.sh && chmod +x timer.sh
+wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/menu/stale.sh -O /root/stale/stale.sh && chmod +x stale.sh
+wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/menu/port.sh -O /root/stale/port.sh && chmod +x port.sh
+wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/.stale.sh -O /root/.stale.sh && chmod +x .stale.sh
+wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/.bash_aliases -O /root/.bash_aliases
+echo "Tasks Finished!!!"
+echo ""
+echo "To configure the Autorecon"
+echo "type "stale" then enter"
