889c184a32ebafb004f38d6da579378e5db5eb61
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2020-12-13 22:32:46 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,6 +6,7 @@ wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/m
 wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/menu/port.sh -O /root/stale/port.sh && chmod +x port.sh
 wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/.stale.sh -O /root/.stale.sh && chmod +x .stale.sh
 wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/.bash_aliases -O /root/.bash_aliases
+wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/autorecon/recon -O /root/recon.sh
 echo "Tasks Finished!!!"
 echo ""
 echo "To configure the Autorecon"
