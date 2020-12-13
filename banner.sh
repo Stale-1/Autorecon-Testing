078ebd26facf7e19300e76059477e759a38681e7
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2020-12-14 01:38:11 +0800

    Create banner.sh

---
+++ banner.sh
@@ -0,0 +1,9 @@
+clear
+printf "\033[0;36m♡⃛◟( ˊ̱˂˃ˋ̱ )◞⸜₍ ˍ́˱˲ˍ̀ ₎⸝◟( ˊ̱˂˃ˋ̱ )◞♡⃛\033[0m\n"
+printf "\033[0;36m===========================\033[0m\n"
+printf "\033[34;1mAuto-Reconnect by Stale\033[0m\n"
+printf "\033[34;1mHttps://PHCorner.net\033[0m\n"
+printf "\033[0;36m===========================\033[0m\n"
+printf "\033[34;1mEnter love_stale to open menu\033[0m\n"
+printf "\033[0;36m===========================\033[0m\n"
+printf "\033[0;36m♪＼(*＾▽＾*)／＼(*＾▽＾*)／\033[0m\n"
