cdc83cf33d998d6039994609e90481331b7c7b1b
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2020-12-13 23:10:25 +0800

    Update timer.sh

--- menu/timer.sh
+++ menu/timer.sh
@@ -7,12 +7,25 @@ do
   echo "===================="
   echo ""
   echo "How many seconds before reconnection?"
-  echo -e "Enter your preferred interval(1-60) \c"
+  echo ""
+  echo "1) Every 30 seconds
+  echo "2) Every 35 seconds
+  echo "3) Every 40 seconds
+  echo "4) Every 45 seconds
+  echo "5) Every 50 seconds
+  echo "6) Every 55 seconds
+  echo "7) Every 60 seconds
+  echo "0) Exit
+  echo -e "Enter your preferred interval \c"
   read answer
   case "$answer" in
-    1) echo "You chose 1" ;;
-    2) echo "You chose 2" ;;
-    3) echo "You chose 2" ;;
-    q) exit ;;
+    1) wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/menu/timer/30 -O /root/stale/.sleep.sh && exit ;;
+    2) wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/menu/timer/35 -O /root/stale/.sleep.sh && exit ;;
+    3) wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/menu/timer/40 -O /root/stale/.sleep.sh && exit ;;
+    4) wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/menu/timer/45 -O /root/stale/.sleep.sh && exit ;;
+    5) wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/menu/timer/50 -O /root/stale/.sleep.sh && exit ;;
+    6) wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/menu/timer/55 -O /root/stale/.sleep.sh && exit ;;
+    7) wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/menu/timer/60 -O /root/stale/.sleep.sh && exit ;;
+    0) exit ;;
   esac
 done
