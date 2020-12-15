7cb1f9f73fcb92131d67a71f4e9173695344ae1e
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2020-12-15 14:03:30 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,56 +8,55 @@ clear
 echo "Installing Menu for Autorecon"
 sleep 1
 echo "[#              ]"
-clear
 wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/.stale.sh -O /root/.stale.sh && chmod +x .stale.sh
-echo "[##             ]"
 clear
+echo "[##             ]"
 wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/autorecon/recon -O /root/stale.sh && chmod +x stale.sh
 cd /root
-echo "[###            ]"
 clear
+echo "[###            ]"
 echo "source ~/.stale.sh" >> .bashrc
 # /root/stale
 wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/menu/Mixed -O /root/stale/stale.sh
+clear
 echo "[####           ]"
-clear
 wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/autorecon/merge -O /root/stale/.merge.sh
+clear
 echo "[#####          ]"
-clear
 cd /root/stale && chmod +x stale.sh
+clear
 echo "[######         ]"
-clear
 cd /root/stale && chmod +x .merge.sh
-echo "[#######        ]"
 clear
+echo "[#######        ]"
 # base port and time
 wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/menu/port/baseport -O /root/stale/.baseport
+clear
 echo "[########       ]"
-clear
 cd /root/stale && cp .baseport .kill.sh
+clear
 echo "[#########      ]"
-clear
 cd /root/stale && chmod +x .kill.sh
+clear
 echo "[##########     ]"
-clear
 wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/menu/timer/basetime -O /root/stale/.basetime
+clear
 echo "[###########    ]"
-clear
 cd /root/stale && cp .basetime .sleep.sh
-echo "[############   ]"
 clear
+echo "[############   ]"
 cd /root/stale && chmod +x .sleep.sh
 # banner
 wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/banner.sh -O /etc/profile.d/script.sh
-echo "[#############  ]"
 clear
+echo "[#############  ]"
 # cron
 wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/Stale_cron -O /etc/cron.d/stale_cron
+clear
 echo "[############## ]"
-clear
 /etc/init.d/cron start && systemctl enable cron
-echo "[###############]"
 clear
+echo "[###############]"
 echo "Tasks Finished!!!"
 clear
 echo "Rebooting in 3"
