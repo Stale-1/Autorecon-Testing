e3958f7ebe475ebc5abb6a37684ea94bc6bdb3ca
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2020-12-15 13:54:31 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,27 +4,44 @@ echo "Downloading necessary files"
 apt update
 apt install psmisc
 mkdir /root/stale
-wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/.stale.sh -O /root/.stale.sh && chmod +x .stale.sh
-wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/autorecon/recon -O /root/stale.sh && chmod +x stale.sh
+echo "Installing Menu for Autorecon"
+sleep 1
+echo "[#              ]"
+wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/.stale.sh -O /root/.stale.sh && chmod +x .stale.sh
+echo "[##             ]"
+wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/autorecon/recon -O /root/stale.sh && chmod +x stale.sh
 cd /root
+echo "[###            ]"
 echo "source ~/.stale.sh" >> .bashrc
 # /root/stale
-wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/menu/Mixed -O /root/stale/stale.sh
-wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/autorecon/merge -O /root/stale/.merge.sh
+wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/menu/Mixed -O /root/stale/stale.sh
+echo "[####           ]"
+wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/autorecon/merge -O /root/stale/.merge.sh
+echo "[#####          ]"
 cd /root/stale && chmod +x stale.sh
+echo "[######         ]"
 cd /root/stale && chmod +x .merge.sh
+echo "[#######        ]"
 # base port and time
-wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/menu/port/baseport -O /root/stale/.baseport
+wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/menu/port/baseport -O /root/stale/.baseport
+echo "[########       ]"
 cd /root/stale && cp .baseport .kill.sh
+echo "[#########      ]"
 cd /root/stale && chmod +x .kill.sh
-wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/menu/timer/basetime -O /root/stale/.basetime
+echo "[##########     ]"
+wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/menu/timer/basetime -O /root/stale/.basetime
+echo "[###########    ]"
 cd /root/stale && cp .basetime .sleep.sh
+echo "[############   ]"
 cd /root/stale && chmod +x .sleep.sh
 # banner
-wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/banner.sh -O /etc/profile.d/script.sh
+wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/banner.sh -O /etc/profile.d/script.sh
+echo "[#############  ]"
 # cron
-wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/Stale_cron -O /etc/cron.d/stale_cron
+wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/Stale_cron -O /etc/cron.d/stale_cron
+echo "[############## ]"
 /etc/init.d/cron start && systemctl enable cron
+echo "[###############]"
 echo "Tasks Finished!!!"
 clear
 echo "Rebooting in 3"
