af065959d76bbfcfbf4f1208497705ac9cbda551
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2020-12-15 10:37:53 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,25 +4,24 @@ echo "Downloading necessary files"
 apt update
 apt install psmisc
 mkdir /root/stale
-wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/.stale.sh -O /root/.stale.sh && chmod +x .stale.sh
-wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/.bash_aliases -O /root/.bash_aliases
-wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/autorecon/recon -O /root/stale.sh && chmod +x stale.sh
+wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/.stale.sh -O /root/.stale.sh && chmod +x .stale.sh
+wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/autorecon/recon -O /root/stale.sh && chmod +x stale.sh
+cd /root
+echo "source ~/.stale.sh" >> .bashrc
 # /root/stale
-wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/menu/timer.sh -O /root/stale/timer.sh
-wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/menu/stale.sh -O /root/stale/stale.sh
-wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/menu/port.sh -O /root/stale/port.sh
-wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/autorecon/sleep -O /root/stale/.sleep.sh
-wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/autorecon/kill -O /root/stale/.kill.sh
-wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/autorecon/merge -O /root/stale/.merge.sh
+wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/menu/timer.sh -O /root/stale/timer.sh
+wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/menu/stale.sh -O /root/stale/stale.sh
+wget https://github.com/Stale-1/Autorecon-Testing/blob/main/menu/port.sh -O /root/stale/port.sh
+wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/autorecon/merge -O /root/stale/.merge.sh
 cd /root/stale && chmod +x timer.sh
 cd /root/stale && chmod +x stale.sh
 cd /root/stale && chmod +x port.sh
-cd /root/stale && chmod +x .sleep.sh
-cd /root/stale && chmod +x .kill.sh
 cd /root/stale && chmod +x .merge.sh
-echo "source ~/.stale.sh" >> .bashrc
-wget https://raw.githubusercontent.com/Stale-1/Auto-Reconnect-of-Dropbear/main/banner.sh -O /etc/profile.d/script.sh
-rm installer.sh
+# base port and time
+wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/menu/port/baseport -O /root/stale/.baseport
+wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/menu/timer/basetime -O /root/stale/.basetime
+# banner
+wget https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/banner.sh -O /etc/profile.d/script.sh
 echo "Tasks Finished!!!"
 clear
 echo "Rebooting in 3"
