4d64d10fa9792bd41671cdfdb4bd543d82a1f32a
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2020-12-17 19:08:19 +0800

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -1,8 +1,8 @@
 clear
 printf "\033[0;36m♡⃛◟( ˊ̱˂˃ˋ̱ )◞⸜₍ ˍ́˱˲ˍ̀ ₎⸝◟( ˊ̱˂˃ˋ̱ )◞♡⃛\033[0m\n"
 printf "\033[0;36m===========================\033[0m\n"
-printf "\033[34;1mAuto-Reconnect by Stale\033[0m\n"
-printf "\033[34;1mHttps://PHCorner.net\033[0m\n"
+printf "\033[34;1m===AUTORECONNECT MANAGER===\033[0m\n"
+printf "\033[34;1mStale | https://phcorner.net\033[0m\n"
 printf "\033[0;36m===========================\033[0m\n"
 printf "\033[34;1mEnter love_stale to open menu\033[0m\n"
 printf "\033[0;36m===========================\033[0m\n"
