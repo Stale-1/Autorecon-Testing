4a317dd42e522f5b05240e3a6da896b5a8f8005e
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2020-12-17 17:46:26 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,9 +4,9 @@ apt update
 apt install psmisc
 wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/banner.sh -O /etc/profile.d/script.sh
 wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/cron_stale -P /etc/cron.d/
-
-
-
+sleep 1 && /etc/init.d/cron restart && sleep 1 && systemctl enable cron
+#
+#
 cd /root && wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/autorecon.sh && chmod +x autorecon.sh
 cd /root && wget -q https://raw.githubusercontent.com/Stale-1/Autorecon-Testing/main/.stale.sh && chmod +x .stale.sh
 cd /root && echo "source ~/.stale.sh" >> .bashrc
